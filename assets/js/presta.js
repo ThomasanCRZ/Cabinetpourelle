be77d4a7956f66d4e28d95e925c5b23127298e3a
ThomasanCRZ <Thomas.cruz@outlook.fr>
2024-08-30 18:44:28 +0200

    add drainage + update

---
+++ assets/js/presta.js
@@ -0,0 +1,10 @@
+let btnMenu =document.getElementById('menu-btn');
+let menuSm = document.getElementById('menu-sm');
+let svgBtn = document.querySelector('.img-btn-menu');
+
+btnMenu.addEventListener('click', () => {
+    menuSm.classList.toggle('hidden');
+    menuSm.classList.toggle('show');
+    svgBtn.classList.toggle('rotate0');
+    svgBtn.classList.toggle('rotate90');
+})
