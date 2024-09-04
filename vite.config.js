be110f170b25cf34423e55156c46a869bd3fecdf
ThomasanCRZ <Thomas.cruz@outlook.fr>
2024-09-04 21:14:49 +0200

    add rooting

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         drainage: resolve(__dirname, 'assets/pages/drainage-lymphatique.html'),
         kobido: resolve(__dirname, 'assets/pages/kobido.html'),
+        hydraface: resolve(__dirname, 'assets/pages/hydraface.html'),
         soinventre: resolve(__dirname, 'assets/pages/soin-du-ventre.html'),
+        reflexologie: resolve(__dirname, 'assets/pages/reflexologie-plantaire.html'),
         tarifs: resolve(__dirname, 'assets/pages/tarifs.html'),
       }
     }
