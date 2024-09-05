b77bc1874be4cf87479b39aeff25b9b9eb4db6b7
ThomasanCRZ <Thomas.cruz@outlook.fr>
2024-09-05 15:53:51 +0200

    Change color + rooting

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,11 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         drainage: resolve(__dirname, 'assets/pages/drainage-lymphatique.html'),
-        kobido: resolve(__dirname, 'assets/pages/kobido.html'),
         hydraface: resolve(__dirname, 'assets/pages/hydraface.html'),
-        soinventre: resolve(__dirname, 'assets/pages/soin-du-ventre.html'),
+        kobido: resolve(__dirname, 'assets/pages/kobido.html'),
         reflexologie: resolve(__dirname, 'assets/pages/reflexologie-plantaire.html'),
+        soinventre: resolve(__dirname, 'assets/pages/soin-du-ventre.html'),
+        spacapilaire: resolve(__dirname, 'assets/pages/spa-capilaire.html'),
         tarifs: resolve(__dirname, 'assets/pages/tarifs.html'),
       }
     }
